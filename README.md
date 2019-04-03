107751218e04d6789704c8783e121d878b9924d9
akilesh <9027560+Akilesh1989@users.noreply.github.com>
2019-04-03 17:37:34 +0800

    minor modifications

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This repository is a compilation of useful configuration files.
+
+To use the envrc_config file rename it to .envrc 
--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
 # function to use export in fsh
 function export
-    if [ $argv ] 
+    if [ $argv ]
         set var (echo $argv | cut -f1 -d=)
         set val (echo $argv | cut -f2 -d=)
         set -g -x $var $val
@@ -15,15 +15,19 @@ export LANG=en_US.UTF-8
 # set the path to the directory that is used most often
 set -g CDPATH . ~ /Users/akilesh/Documents/
 
+# some useful shortcuts that I use
 alias ll='ls -al'
 alias gl='git log --graph --pretty="format:%C(yellow)%h%Cblue%d%Creset %s %C(white)"'
-
+alias gs="git status"
+alias ga="git add -p"
+alias gc="git commit"
+alias gaa="git add ."
 alias ssh_tunnel='ssh -f -N ss-r'
 alias zc='zcat <'
-
-alias find_file='find . -name'
+alias findfile='find . -name'
 
 # direnv
 eval (direnv hook fish)
 
-export HOME=/Users/akilesh/
+# set ls color to teal from dark blue
+set -Ux LSCOLORS gxfxbEaEBxxEhEhBaDaCaD
