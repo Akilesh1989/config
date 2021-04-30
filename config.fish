1794fc2ffcdeea1adb803b480f550878085b42e4
Akilesh1989 <9027560+Akilesh1989@users.noreply.github.com>
2021-04-30 12:48:51 +0530

    Update config.fish

--- config.fish
+++ config.fish
@@ -30,13 +30,6 @@ export HADOOP_HOME=/usr/local/Cellar/hadoop/3.3.0/libexec
 export HIVE_HOME=/usr/local/Cellar/hive/2.7.1/libexec
 
 
-# work ssh
-alias ds9="ssh -p 3999 -L 2001:192.168.1.222:8888 -L 2002:192.168.1.221:10000 -L 2003:192.168.1.221:9083 -L 2000:192.168.1.118:8088 -L 3000:192.168.1.118:8080 akilesh@10.132.184.209"
-alias hue="ssh -p 3999 -L 2001:192.168.1.222:8888 -L 2002:192.168.1.221:10000 -L 2003:192.168.1.221:9083 -L 2000:192.168.1.117:8088 akilesh@10.132.184.209"
-alias sgc="ssh akilesh@10.11.2.17 -i ~/.ssh/id_rsa_hadoop"
-alias sgcp="ssh prod-testing@10.111.2.17"
-alias mensa="ssh -i "/Users/akilesh/Documents/work/mensa/daas-key-pair.pem" ec2-user@ec2-52-221-89-169.ap-southeast-1.compute.amazonaws.com"
-alias mensap="ssh akilesh@10.11.2.17"
 export PATH="~/.pyenv/bin:$PATH"
 pyenv init - | source
 # eval '(pyenv virtualenv-init -)'
